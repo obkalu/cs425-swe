8e389a5cd2e0cea4bf846221a616f5ffc48d1e41
okalu <okalu1@outlook.com>
2020-05-28 00:24:13 -0500

    Added new maven project with CI yaml config for github actions workflow

---
+++ studentrecordsmgmtconsoleapp/src/main/java/edu/miu/cs/cs425/githubactionsdemo/studentrecordsmgmtconsoleapp/App.java
@@ -0,0 +1,12 @@
+package edu.miu.cs.cs425.githubactionsdemo.studentrecordsmgmtconsoleapp;
+
+/**
+ * Hello world!
+ */
+public class App {
+	
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+    
+}
